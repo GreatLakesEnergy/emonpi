e9444ae0756c51521272909d9b81c86c8f7480bb
Alp Tilev <atilev@gmail.com>
2016-07-19 16:29:23 +0200

    adding ui status

--- lcd/emonPiLCD.py
+++ lcd/emonPiLCD.py
@@ -56,7 +56,7 @@ default = dict(
     SHUTDOWN_TIME = 3,
     GPIO_PORT = 'P8_11',
     GPIO_PORT_shutdown = 'P8_12',
-    max_number_pages = 6,
+    max_number_pages = 7,
 
     host = 'localhost',
     port = 6379,
@@ -93,18 +93,15 @@ def get_config( str ):
     try:
         val = configs.get('emonPiLCD', str)
     except:
-        #print 'no value nor default value, check name or add to default configuration'
+        print 'no value nor default value, check name or add to default configuration'
         return None
 
     val = remove_comments( val )
     try:
-        integer =  int(val)
+        val =  int(val)
     except:
-        #print error
-        integer = val
+	pass
     
-    val = integer
-
     if val == 'True' or val == 'true':
         val = True
     elif val == 'False' or val == 'false':
@@ -116,8 +113,12 @@ def get_config( str ):
 # -----------------------------
 # removes comments on line strings
 def remove_comments(string):
-    arr = string.split()
-    return arr[0]
+    try:
+	    string = string.split()
+    except:
+            string = [string] 
+            pass
+    return string[0]
 
 
 # ################################################
@@ -160,7 +161,7 @@ GPIO.setup( GPIO_PORT_shutdown,GPIO.IN)
 new_switch_state = GPIO.input(GPIO_PORT)
 shutdown_button =GPIO.input(GPIO_PORT_shutdown)
 
-max_number_pages = get_config('max_number_pages')  #6
+max_number_pages = get_config('max_number_pages')  #7
 
 SHUTDOWN_TIME = get_config('SHUTDOWN_TIME')        #3  # Shutdown after 3 second press
 
@@ -187,7 +188,7 @@ else:
 loghandler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s %(message)s'))
 logger = logging.getLogger("emonPiLCD")
 logger.addHandler(loghandler)
-logger.setLevel(logging.INFO)    # >> config??
+logger.setLevel(logging.DEBUG)    # >> config??
 
 logger.info("emonPiLCD Start")
 
@@ -226,6 +227,7 @@ class Background(threading.Thread):
         logger.info("Starting background thread")
         # Loop until we stop is false (our exit signal)
 
+	logger.info("Running with configuration %s"%max_number_pages)
         pppactive = 0
         wlanactive = 0
         ethactive = 0
@@ -255,13 +257,15 @@ class Background(threading.Thread):
 
 	# Ethernet
 	# --------------------------------------------------------------------------------
-			eth0 = "ip addr show eth0 | grep inet | awk '{print $2}' | cut -d/ -f1 | head -n1"
+			eth0 = "ip addr show eth0 | grep inet | grep -v inet6 | awk '{print $2}' | cut -d/ -f1 | head -n1"
 			p = Popen(eth0, shell=True, stdout=PIPE)
 			eth0ip = p.communicate()[0][:-1]
 
 			ethactive = 1
+			# Ignore all cases where IPv4 is not there
 			if eth0ip=="" or eth0ip==False:
 			    ethactive = 0
+
 
 			r.set("eth:active",ethactive)
 			r.set("eth:ip",eth0ip)
@@ -348,7 +352,7 @@ class Background(threading.Thread):
 			logger.info("background: wlan "+str(signallevel))
 
 			gsm_signallevel = 0
-
+		
 		    if (now - last5h) >= 18000.0:
 			last5h = now
 			if pppactive:
@@ -421,6 +425,23 @@ def shutdown():
         lcd.backlight(0) 	# backlight zero must be the last call to the LCD to keep the backlight off
         call('reboot', shell=False)	#
         sys.exit() #end script
+
+def restart_ethernet():
+	"""
+ 	Intiate thernet restart. TODO: move this to monit	
+	"""
+	if_down = "ifdown eth0"
+	if_up = "ifup eth0"
+
+	logger.debug("Trying to  restart ethernet")			
+	p = Popen(if_down, shell=True, stdout=PIPE)
+	ppp0ip = p.communicate()[0][:-1]
+	time.sleep(10)
+	logger.debug("Trying to renable ethernet")			
+	p = Popen(if_up, shell=True, stdout=PIPE)
+	ppp0ip = p.communicate()[0][:-1]
+
+
 
 def get_uptime():
 
@@ -541,7 +562,7 @@ while 1:
         if backlight == True: page = page + 1
         if page > max_number_pages: page = 0
         buttonPress_time = time.time()
-
+	logger.info("On Page %s of total %s"%(page,max_number_pages))
         #turn backight off afer x seconds
     if (now - buttonPress_time) > backlight_timeout:
         backlight = False
@@ -560,6 +581,10 @@ while 1:
             if int(r.get("eth:active")):
                 lcd_string1 = "Ethernet: YES"
                 lcd_string2 = r.get("eth:ip")
+            elif int(r.get("eth:active")) == 2:
+		restart_ethernet()
+		logger.warning("Ethernet IP is in IPv6 will try to renew dhcp")
+		r.set("eth:active",0)
             else:
                 # AT - Removing int casting. not necassary
                 if r.get("wlan:active"):
@@ -636,7 +661,18 @@ while 1:
                 lcd_string1 = 'Data Sent : ...'
                 lcd_string2 = 'Data Rec. : ...'
              #   print"*******************power 3:....."
-             #   print"********************power 4:......"
+             #   print"********************power 4:......
+
+        elif page==7:
+            tx = r.get("server:active")
+
+            if tx is not 0:
+                lcd_string1 = 'Server Com.  '
+                lcd_string2 = 'Established'
+
+            else:
+                lcd_string1 = 'Server Com.  '
+                lcd_string2 = 'Not Established'
 
 
 
